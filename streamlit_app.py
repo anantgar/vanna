e84c2904b60618a9ac03df5909963a91425cefce
Anant Garg <112724288+anantgar@users.noreply.github.com>
2025-01-17 07:55:31 +0000

    integration with vanna

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,44 @@
 import streamlit as st
-from openai import OpenAI
+import redshift_connector
+from vanna.openai import OpenAI_Chat
+from vanna.vannadb import VannaDB_VectorStore
+import pandas as pd
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+st.title("💬 Talk to Your Data")
+
+openai_api_key = st.secrets["openai_api_key"]
+
+conn = redshift_connector.connect(
+    database=st.secrets["database"],
+    user=st.secrets["user"],
+    password=st.secrets["db_password"],
+    host=st.secrets["host"],
+    port=5439
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+def run_sql(sql: str) -> pd.DataFrame:
+    df = pd.read_sql_query(sql, conn)
+    return df
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+class MyVanna(VannaDB_VectorStore, OpenAI_Chat):
+    def __init__(self, config=None):
+        VannaDB_VectorStore.__init__(self, vanna_model=st.secrets["vanna_model"], vanna_api_key=st.secrets["vanna_api_key"], config=config)
+        OpenAI_Chat.__init__(self, config=config)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+vn = MyVanna(
+    config={'api_key': openai_api_key,
+            'model': 'gpt-4o'}
+)
+vn.run_sql = run_sql
+vn.run_sql_is_set = True
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# Input field for the user to ask a question
+my_question = st.text_input("Ask me a question about your data:")
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+# Check if the user has entered a question
+if my_question:
+    st.text(my_question)
+    sql = vn.generate_sql(my_question)
+    st.text(sql)
+    df = vn.run_sql(sql)    
+    st.dataframe(df, use_container_width=True)
